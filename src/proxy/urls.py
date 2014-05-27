2d3f7ae971843170a13b02d3f3b1feda5d985b0e
Gust <darksshades@hotmail.com>
2014-05-27 09:37:04 -0300

    Change default values and proxy url

--- src/proxy/urls.py
+++ src/proxy/urls.py
@@ -9,6 +9,9 @@ urlpatterns = patterns('',
     url(r'^(?P<path>(?:admin|wiki|changeset|newticket|ticket|chrome|timeline|roadmap|browser|report|tags|query|about|prefs|log|attachment|raw-attachment|diff|milestone).*)$',
         TracProxyView.as_view()),
 
+    # Trac extra URLs
+    url(r'^trac/(?P<path>.*)$', TracProxyView.as_view()),
+
     # Jenkins URLs
     url(r'^ci/(?P<path>.*)$', JenkinsProxyView.as_view()),
 
@@ -16,7 +19,13 @@ urlpatterns = patterns('',
     url(r'^(?P<path>(?:users|dashboard|profile).*)$',
         GitlabProxyView.as_view()),
 
-    #  URLs
+    # Gitlab extra URLs
+    url(r'^gitlab/(?P<path>.*)$', GitlabProxyView.as_view()),
+
+    # Redmine extra URLs
+    url(r'^redmine/(?P<path>.*)$', RedmineProxyView.as_view()),
+
+    # Redmine URLs
     url(r'^(?P<path>(?:projects).*)$',
         RedmineProxyView.as_view()),
 )
